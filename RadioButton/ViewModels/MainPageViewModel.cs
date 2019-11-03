f3eb080ce5586531cec84eb8dcbe1f9d67adc50e
ats-y <london.rightside@gmail.com>
2019-11-03 15:28:25 +0900

    ViewModelにチェックボックスの値をバインディング。

--- RadioButton/ViewModels/MainPageViewModel.cs
+++ RadioButton/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Diagnostics;
+using Prism.Navigation;
+using Reactive.Bindings;
+
 namespace RadioButton.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+		public ReactiveProperty<bool> IsChecked1 { get; set; } = new ReactiveProperty<bool>();
+        public ReactiveProperty<bool> IsChecked2 { get; set; } = new ReactiveProperty<bool>();
+        public ReactiveProperty<bool> IsChecked3 { get; set; } = new ReactiveProperty<bool>();
+        public ReactiveCommand TestCommand { get; set; }
+
         public MainPageViewModel()
         {
+            TestCommand = new ReactiveCommand();
+            TestCommand.Subscribe( _=>
+            {
+                Debug.WriteLine("Testボタンタップ");
+            });
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            IsChecked2.Value = true;
         }
     }
 }
